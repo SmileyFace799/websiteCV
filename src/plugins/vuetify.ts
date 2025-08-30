132c314c173fc0d24a9868b180dde50da9a140b8
SmileyFace799 <Henrikgran85@gmail.com>
2025-08-30 13:45:23 +0200

    Added vuetify

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,12 @@
+
+import { createVuetify } from 'vuetify'
+import { mdi } from 'vuetify/iconsets/mdi'
+
+export const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi,
+    },
+  },
+})
