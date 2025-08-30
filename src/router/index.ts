d7336d540a056636ac26c177f8d45498bb97061f
SmileyFace799 <Henrikgran85@gmail.com>
2025-08-30 11:16:47 +0200

    First commit 😎

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '../components/HelloWorld.vue'
+
+export default createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{ path: '/', name: 'Home', component: Home },
+	]
+})
